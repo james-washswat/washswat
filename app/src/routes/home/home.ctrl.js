ea6ca391947fc732d01379e4e6d4ba6ccf5001e1
podomat <podomat.ex@gmail.com>
2022-03-20 15:39:33 +0900

    Web UI 기본 frame 작업

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,19 @@
 "use strict";
 
+const logger = require("../../config/logger");
+const Order = require("../../model/order");
+
 const output = {
-    home: (req, res) => {
-        res.render("home/index.ejs");
-    }
+    home: async (req, res) => {
+        const order = new Order();
+        const response = await order.list(2);
+
+        console.log(response);
+
+        if (response.success) {
+            res.render("home/index.ejs", response);
+        }
+    },
 }
 
 module.exports = {
