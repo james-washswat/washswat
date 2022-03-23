e398d35d3852bce6ec5c8ab4e597ccaf607ab972
podomat <podomat.ex@gmail.com>
2022-03-23 23:36:52 +0900

    불필요한 모듈 로드 제거

--- services/controllers/controller.js
+++ services/controllers/controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const properties = require('../package.json');
-const distance = require('../service/distance');
 const Order = require('../models/order');
 const logger = require('../config/logger');
 
